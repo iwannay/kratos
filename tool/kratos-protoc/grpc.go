99e03ea367e9c7ab74d8c14adfbfae4cdc2dbd2d
iwannay <772648576@qq.com>
2020-11-12 09:45:36 +0800

    调整grpc代码生成器

--- tool/kratos-protoc/grpc.go
+++ tool/kratos-protoc/grpc.go
@@ -7,6 +7,9 @@ import (
 const (
 	_getGRPCGen = "go get -u github.com/gogo/protobuf/protoc-gen-gofast"
 	_grpcProtoc = `protoc --proto_path=%s --proto_path=%s --proto_path=%s --gofast_out=plugins=grpc:.`
+
+	_getGRPCGenOfficial = "go get -u github.com/golang/protobuf/protoc-gen-go"
+	_grpcProtocOfficial = `protoc --proto_path=%s --proto_path=%s --proto_path=%s --go_out=plugins=grpc:.`
 )
 
 func installGRPCGen() error {
@@ -15,9 +18,14 @@ func installGRPCGen() error {
 			return err
 		}
 	}
+	if _, err := exec.LookPath("protoc-gen-go"); err != nil {
+		if err := goget(_getGRPCGenOfficial); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
 func genGRPC(files []string) error {
-	return generate(_grpcProtoc, files)
+	return generate(_grpcProtocOfficial, files)
 }
